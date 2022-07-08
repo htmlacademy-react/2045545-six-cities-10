32c1c5de7976bbabe6d4c8f80b1c88cbac8207e0
Elena <eamatytsina@gmail.com>
2022-07-08 22:40:22 +0300

    добавлены компоненты страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppScreenProps = {
+  placeCardsCount: number;
+}
+
+function App({placeCardsCount}: AppScreenProps): JSX.Element {
+  return (<MainScreen placeCardsCount={placeCardsCount}/>);
 }
 
 export default App;
